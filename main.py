ccd1a2957638f554103646c98b66b2e2f21ff540
TheTasak <horushom@gmail.com>
2024-12-07 20:59:29 +0100

    Added request delay and output type options

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ import json
 import os
 import sys
 import argparse
+import time
 
+import pandas as pd
 import requests as req
 from bs4 import BeautifulSoup
+from pathlib import Path
 
+data_folder = "data"
 types = [
     "raporty-finansowe-rachunek-zyskow-i-strat",
     "raporty-finansowe-bilans",
@@ -20,67 +24,79 @@ types = [
 
 
 def handle_path(path):
-    if not os.path.exists(path):
-        try:
-            os.mkdir(path)
-        except OSError as error:
-            print(error)
-            return False
-    return True
+    handle = Path(path)
+    handle.mkdir(parents=True, exist_ok=True)
 
 
-def get_price_data(stock, iters):
-    if not handle_path(stock):
-        return
+def get_price_data(stock, delay, iters):
+    stock_dir = f'{data_folder}/{stock}'
+    handle_path(stock_dir)
 
     for i in range(0, iters):
         url = f'https://www.biznesradar.pl/notowania-historyczne/{stock},{i+1}'
         webdata = req.get(url)
         if not webdata.ok:
             print("Could not get webdata")
-            exit(400)
+            return
 
         soup = BeautifulSoup(webdata.text, 'lxml')
         table = soup.find("table", class_="qTableFull")
-        with open(f'{stock}/data_price_{stock}_{i}.txt', "w") as file:
+        if table is None:
+            return
+        with open(f'{stock_dir}/data_price_{stock}_{i}.txt', "w") as file:
             file.write(str(table))
+        time.sleep(delay/1000)
     
 
-def transform_price_data(stock, iters):
-    if not os.path.exists(stock):
+def transform_price_data(stock, iters, output):
+    stock_dir = f'{data_folder}/{stock}'
+    if not os.path.exists(stock_dir):
         raise Exception("Directory with stock data doesn't exist")
         return
 
     all_data = []
     header_data = []
     for i in range(0, iters):
-        with open(f'{stock}/data_price_{stock}_{i}.txt', "r") as file:
-            data = file.read()
-            soup = BeautifulSoup(data, 'lxml')
-            if i == 0:
-                headers = soup.find("tr").find_all("th")
-                for header in headers[:]:
-                   header_data.append(header.text) 
-            rows = soup.find_all("tr")[1:]
-            for row in rows:
-                row_data = []
-                for cell in row.find_all("td"):
-                    row_data.append(cell.text.replace(" ", ""))
-                all_data.append(row_data)
+        try:
+            file = open(f'{stock_dir}/data_price_{stock}_{i}.txt', "r")  
+        except:
+            break
+        data = file.read()
+        file.close()
+        soup = BeautifulSoup(data, 'lxml')
+        if i == 0:
+            headers = soup.find("tr").find_all("th")
+            for header in headers[:]:
+               header_data.append(header.text) 
+        rows = soup.find_all("tr")[1:]
+        for row in rows:
+            row_data = []
+            for cell in row.find_all("td"):
+                row_data.append(cell.text.replace(" ", ""))
+            all_data.append(row_data)
 
-    json_data = []
+    if output == "json":
+        json_data = []
 
-    for value in all_data:
-        obj = {}
-        for index, cell in enumerate(value):
-            obj[header_data[index]] = cell
-        json_data.append(obj)
+        for value in all_data:
+            obj = {}
+            for index, cell in enumerate(value):
+                obj[header_data[index]] = cell
+            json_data.append(obj)
 
-    with open(f'{stock}/{stock}_price.txt', "w", encoding='utf-8') as file:
-        file.write(json.dumps(json_data, indent=3, ensure_ascii=False))
+        with open(f'{stock_dir}/{stock}_price.txt', "w", encoding='utf-8') as file:
+            file.write(json.dumps(json_data, indent=3, ensure_ascii=False))
+    elif output == "csv":
+        data_dict = {}
+        for index, value in enumerate(all_data):
+            data_dict[index] = value
+        df = pd.DataFrame.from_dict(data_dict, orient="index", columns=header_data)
+        df.to_csv(f'{stock_dir}/{stock}_price.csv', index=False)
 
 
-def get_financial_data(data_type, stock):
+def get_financial_data(data_type, stock, delay):
+    stock_dir = f'{data_folder}/{stock}'
+    handle_path(stock_dir)
     url = f'https://www.biznesradar.pl/{data_type}/{stock}'
 
     webdata = req.get(url)
@@ -90,22 +106,19 @@ def get_financial_data(data_type, stock):
 
     soup = BeautifulSoup(webdata.text, 'lxml')
     table = soup.find("table", class_="report-table")
-    if not os.path.exists(stock):
-        try:
-            os.mkdir(stock)
-        except OSError as error:
-            print(error)
-    with open(f'{stock}/data_{stock}.txt', "w") as file:
+    with open(f'{stock_dir}/data_{stock}.txt', "w") as file:
         file.write(str(table))
+    time.sleep(delay/1000)
 
 
-def transform_financial_data(data_type, stock):
+def transform_financial_data(data_type, stock, output):
+    stock_dir = f'{data_folder}/{stock}'
     all_data = []
     headers_data = ["Rok"]
-    if not os.path.exists(stock):
+    if not os.path.exists(stock_dir):
         raise Exception("Directory with stock data doesn't exist")
         return
-    with open(f'{stock}/data_{stock}.txt', "r") as file:
+    with open(f'{stock_dir}/data_{stock}.txt', "r") as file:
         data = file.read()
     soup = BeautifulSoup(data, 'lxml')
 
@@ -123,33 +136,48 @@ def transform_financial_data(data_type, stock):
         row_data.append(header.text)
         for cell in cells[1:-1]:
             value = cell.find(class_="value")
-            row_data.append(value.text if value else "")
+            row_data.append(value.text.replace(" ", "") if value else "")
         all_data.append(row_data)
 
-    json_data = []
+    if output == "json":
+        json_data = []
 
-    for index, date in enumerate(headers):
-        obj = {}
-        for value in all_data:
-            obj[value[0]] = value[index + 1].replace(" ", "")
-        json_data.append(obj)
+        for index, date in enumerate(headers):
+            obj = {}
+            for value in all_data:
+                obj[value[0]] = value[index + 1].replace(" ", "")
+            json_data.append(obj)
 
-    with open(f'{stock}/{stock}_{data_type}.txt', "w", encoding='utf-8') as file:
-        file.write(json.dumps(json_data, indent=3, ensure_ascii=False))
+        with open(f'{stock_dir}/{stock}_{data_type}.txt', "w", encoding='utf-8') as file:
+            file.write(json.dumps(json_data, indent=3, ensure_ascii=False))
+    elif output == "csv":
+        data_dict = {}
+        for index, value in enumerate(all_data[1:]):
+            data_dict[index] = value
+        df = pd.DataFrame.from_dict(data_dict, orient="index", columns=all_data[0])
+        df.to_csv(f'{stock_dir}/{stock}_{data_type}.csv', index=False)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("BiznesRadar Scrapper")
     parser.add_argument("--type", help="Specify type of data which you want to scrap. Available options: price, financial", type=str)
-    parser.add_argument("--stock", help="Specify stock to scrap", type=str)
-    parser.add_argument("--iters", nargs="?", help="Number of iterations when scrapping price", type=int, default=5)
+    parser.add_argument("--stock", help="Specify stocks to scrap, delimited by a comma", type=str)
+    parser.add_argument("--max_iters", nargs="?", help="Maximum number of iterations when scrapping price", type=int, default=5)
+    parser.add_argument("--delay", nargs="?", help="Delay after making each web request (in milliseconds)", type=int, default=100)
+    parser.add_argument("--output", nargs="?", help="Output type. Available options: json, csv", type=str, default="json")
     args = parser.parse_args()
-    if args.type == "financial":
-        for data_type in types:
-          get_financial_data(data_type, args.stock)
-          transform_financial_data(data_type, args.stock)
-    elif args.type == "price":
-        get_price_data(args.stock, args.iters)
-        transform_price_data(args.stock, args.iters)
+    if args.type == "financial" and args.stock is not None:
+        for stock in args.stock.split(","):
+            stock = stock.strip()
+            for data_type in types:
+              get_financial_data(data_type, stock, args.delay)
+              transform_financial_data(data_type, stock, args.output)
+    elif args.type == "price" and args.stock is not None:
+        for stock in args.stock.split(","):
+            stock = stock.strip()
+            get_price_data(stock, args.delay, args.max_iters)
+            transform_price_data(stock, args.max_iters, args.output)
+    elif args.type is None or len(args.type) == 0:
+        print("No type specified. Type -h to view all possible arguments")
     else:
-        print("Wrong type specified")
+        print("Wrong type or stock specified")
